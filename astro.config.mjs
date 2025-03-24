2657115a94dc517d1ffa4616794fd0fec336560a
Minarox <contact@minarox.fr>
2025-03-24 10:18:10 +0100

    build: Add SEO packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import metaTags from 'astro-meta-tags';
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://rltm.minarox.fr",
     base: "/",
     outDir: "./build",
-    integrations: [],
+    integrations: [sitemap(), metaTags(), compressor({ gzip: false })],
     server: {
         host: "127.0.0.1"
     }
