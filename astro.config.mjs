af5271a2bb02bb12a42cb9aba6249a1ecb1907fe
Mathis S.M. <contact@minarox.fr>
2025-03-24 16:33:16 +0100

    build: Add Astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://rltm.minarox.fr",
     base: "/",
     outDir: "./build",
-    integrations: [],
+    integrations: [db()],
     server: {
         host: "127.0.0.1"
     }
